c815352a77da667c13fa2f6b8ace047cae8a81fe
CameronBrenton <Cameron.Brenton@keyin.com>
2021-08-04 12:05:16 -0230

    First commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// Project 1 Cluster 1
+
+const { MongoClient } = require('mongodb');
+
+async function main() {
+	const uri = "mongodb+srv://Cameron:password1001@cluster1.fhszb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+	const client = new MongoClient(uri);
+
+	try {
+		await client.connect();
+		console.log('Successfully connected to mongo db!')
+		await listDatabases(client);
+	}catch(err) {
+		console.error(err);
+	} finally {
+		await client.close();
+	}
+}
+
+main().catch(console.error);
+
+async function createListing(client, newListing) {
+	const result = client.db("sample_airbnb").collection("listingsAndReviews").insertOne(newListing);
+
+	console.log(`New listing created with the following id: ${result.insertedId}`);
+}
+
+async function listDatabases(client) {
+	const databasesList = await client.db().admin().listDatabases();
+
+	console.log("Databases: ");
+	databasesList.databases.forEach(db => {
+		console.log(`- ${db.name}`)
+	})
+}
